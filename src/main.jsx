fc5e243e233bc5a3f55c04d07124523e1c421cbb
bst003 <bstanner33@yahoo.com>
2023-06-27 15:26:44 -0400

    Add Google fotns and set up brand colors

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,12 @@ import RouteSwitch from "./RouteSwitch.jsx";
 import "./assets/scss/base.scss";
 import "normalize.css";
 
+import { initializeApp } from "firebase/app";
+import { getFirebaseConfig } from "./firebaseConfig.js";
+
+const firebaseAppConfig = getFirebaseConfig();
+initializeApp(firebaseAppConfig);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouteSwitch />
