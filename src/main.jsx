cd88e47b0ea16e9b3a6e3d33f7d3286beefe5bbb
bst003 <bstanner33@yahoo.com>
2023-07-04 22:05:19 -0400

    Clean up main jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import RouteSwitch from "./RouteSwitch.jsx";
 
@@ -7,14 +7,12 @@ import "normalize.css";
 
 import { initializeApp } from "firebase/app";
 import { getFirebaseConfig } from "./firebase/config.js";
-// import { initFirebaseAuth } from "./firebase/authentication.js";
 
 const firebaseAppConfig = getFirebaseConfig();
 initializeApp(firebaseAppConfig);
-// initFirebaseAuth();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouteSwitch />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <RouteSwitch />
+  // </React.StrictMode>
 );
