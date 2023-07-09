306c83b975904df10b0ab9c25ead6208ae272ca0
bst003 <bstanner33@yahoo.com>
2023-07-08 23:24:02 -0400

    Begin footer setup

--- src/components/universal/Footer.jsx
+++ src/components/universal/Footer.jsx
@@ -1,11 +1,23 @@
+import { Link } from "react-router-dom";
+
 import "./Footer.scss";
 
 const Footer = () => {
-  return (
-    <footer className="site-footer">
-      <div className="site-footer__inner">Footer</div>
-    </footer>
-  );
+    return (
+        <footer className="site-footer">
+            <div className="site-footer__inner">
+                <div className="site-footer__credits">Filler</div>
+
+                <nav className="site-footer__navigation">
+                    <ul>
+                        <li>
+                            <Link to="/">Test</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
