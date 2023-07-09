faceec0f5410beccd91db7ec57cba7714a90f99f
bst003 <bstanner33@yahoo.com>
2023-07-08 22:59:04 -0400

    Begin styling search bar, add google fonts

--- src/components/universal/SearchBar.jsx
+++ src/components/universal/SearchBar.jsx
@@ -1,8 +1,21 @@
+import "./SearchBar.scss";
+
 const SearchBar = () => {
   return (
-    <div className="site-search-bar">
-      <input className="search-input" type="text" placeholder="Search Posts" />
-    </div>
+    <form className="site-search-bar">
+      <label htmlFor="search">
+        <input
+          id="search"
+          name="search"
+          className="search-input"
+          type="text"
+          placeholder="Search Posts"
+        />
+      </label>
+      <button type="submit">
+        <span className="screen-reader-text">Search</span>
+      </button>
+    </form>
   );
 };
 
