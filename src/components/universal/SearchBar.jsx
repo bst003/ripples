e5ccfc4531403e137d0c3a227db84eed7194dd59
bst003 <bstanner33@yahoo.com>
2023-07-08 23:06:52 -0400

    Add custom prettier file

--- src/components/universal/SearchBar.jsx
+++ src/components/universal/SearchBar.jsx
@@ -1,23 +1,23 @@
 import "./SearchBar.scss";
 
 const SearchBar = () => {
-  return (
-    <form className="site-search-bar">
-      <label htmlFor="search">
-        <input
-          id="search"
-          name="search"
-          className="search-input"
-          type="text"
-          placeholder="Search Posts"
-        />
-        <button type="submit">
-          <i className="fas fa-search"></i>
-          <span className="screen-reader-text">Search</span>
-        </button>
-      </label>
-    </form>
-  );
+    return (
+        <form className="site-search-bar">
+            <label htmlFor="search">
+                <input
+                    id="search"
+                    name="search"
+                    className="search-input"
+                    type="text"
+                    placeholder="Search Posts"
+                />
+                <button type="submit">
+                    <i className="fas fa-search"></i>
+                    <span className="screen-reader-text">Search</span>
+                </button>
+            </label>
+        </form>
+    );
 };
 
 export default SearchBar;
