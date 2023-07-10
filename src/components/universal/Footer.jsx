faa38f0b5d9d5b2d01a567d89ee4b8eb4f9d8290
bst003 <bstanner33@yahoo.com>
2023-07-09 21:28:55 -0400

    Continue styling footer

--- src/components/universal/Footer.jsx
+++ src/components/universal/Footer.jsx
@@ -6,10 +6,18 @@ const Footer = () => {
     return (
         <footer className="site-footer">
             <div className="site-footer__inner">
-                <div className="site-footer__credits">Filler</div>
+                <div className="site-footer__credits">
+                    A project by{" "}
+                    <a href="https://github.com/bst003" target="_blank" rel="noreferrer">
+                        bst003
+                    </a>
+                </div>
 
                 <nav className="site-footer__navigation">
                     <ul>
+                        <li>
+                            <Link to="/">Front Page</Link>
+                        </li>
                         <li>
                             <Link to="/">Test</Link>
                         </li>
