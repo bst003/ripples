3acb90dcd6aa8318eb3a5811e8312abca0bc27d5
bst003 <bstanner33@yahoo.com>
2023-07-09 21:41:17 -0400

    Add Account view to site

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -8,32 +8,34 @@ import UserContext from "./components/universal/UserContext.jsx";
 
 import Home from "./views/Home.jsx";
 import SubRipple from "./views/SubRipple.jsx";
+import Account from "./views/Account.jsx";
 import Profile from "./views/Profile.jsx";
 import NotFound from "./views/NotFound.jsx";
 
 const RouteSwitch = () => {
-  const [user, setUser] = useState(null);
+    const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    initAuthListener(setUser);
-  }, []);
+    useEffect(() => {
+        initAuthListener(setUser);
+    }, []);
 
-  return (
-    <>
-      <UserContext.Provider value={user}>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<MainLayout />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/r/:slug" element={<SubRipple />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="*" element={<NotFound />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </UserContext.Provider>
-    </>
-  );
+    return (
+        <>
+            <UserContext.Provider value={user}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route element={<MainLayout />}>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/r/:slug" element={<SubRipple />} />
+                            <Route path="/account" element={<Account />} />
+                            <Route path="/profile" element={<Profile />} />
+                            <Route path="*" element={<NotFound />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </UserContext.Provider>
+        </>
+    );
 };
 
 export default RouteSwitch;
--- src/components/universal/AccountNav.jsx
+++ src/components/universal/AccountNav.jsx
@@ -1,28 +1,45 @@
 import { useContext } from "react";
 
+import { Link } from "react-router-dom";
+
 import UserContext from "./UserContext.jsx";
 
 import { signInUser, signOutUser } from "../../firebase/authentication.js";
 
 const AccountNav = () => {
-  const user = useContext(UserContext);
+    const user = useContext(UserContext);
 
-  return (
-    <div className="site-acount-nav">
-      {user ? (
-        <>
-          {user.userName}
-          <button type="button" onClick={signOutUser}>
-            Sign Out
-          </button>
-        </>
-      ) : (
-        <button type="button" onClick={signInUser}>
-          Sign In with Google
-        </button>
-      )}
-    </div>
-  );
+    const toggleNav = () => {};
+
+    return (
+        <div className="site-acount-nav">
+            {user ? (
+                <div className="account-nav__contain">
+                    <button className="account-nav__toggle" type="button" onClick={toggleNav}>
+                        {user.userName}
+                    </button>
+
+                    <ul>
+                        <li>
+                            <Link to="/profile">Profile</Link>
+                        </li>
+                        <li>
+                            <Link to="/account">Account</Link>
+                        </li>
+                        <li>
+                            <button type="button" onClick={signOutUser}>
+                                Sign Out
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            ) : (
+                <button type="button" onClick={signInUser}>
+                    Sign In with Google
+                </button>
+            )}
+        </div>
+    );
 };
 
 export default AccountNav;
