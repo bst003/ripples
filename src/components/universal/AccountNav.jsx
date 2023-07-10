125df362bffc226478a0242c72d17d99404b5b33
bst003 <bstanner33@yahoo.com>
2023-07-09 21:55:59 -0400

    Begin styling base buttons and AccountNAv

--- src/components/universal/AccountNav.jsx
+++ src/components/universal/AccountNav.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 import UserContext from "./UserContext.jsx";
 
+import "./AccountNav.scss";
+
 import { signInUser, signOutUser } from "../../firebase/authentication.js";
 
 const AccountNav = () => {
@@ -12,29 +14,35 @@ const AccountNav = () => {
     const toggleNav = () => {};
 
     return (
-        <div className="site-acount-nav">
+        <div className="acount-nav">
             {user ? (
                 <div className="account-nav__contain">
-                    <button className="account-nav__toggle" type="button" onClick={toggleNav}>
+                    <button
+                        className="account-nav__toggle btn-el"
+                        type="button"
+                        onClick={toggleNav}
+                    >
                         {user.userName}
                     </button>
 
-                    <ul>
-                        <li>
-                            <Link to="/profile">Profile</Link>
-                        </li>
-                        <li>
-                            <Link to="/account">Account</Link>
-                        </li>
-                        <li>
-                            <button type="button" onClick={signOutUser}>
-                                Sign Out
-                            </button>
-                        </li>
-                    </ul>
+                    <nav className="account-nav__navigation">
+                        <ul>
+                            <li>
+                                <Link to="/profile">Profile</Link>
+                            </li>
+                            <li>
+                                <Link to="/account">Account</Link>
+                            </li>
+                            <li>
+                                <button type="button" onClick={signOutUser}>
+                                    Sign Out
+                                </button>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
             ) : (
-                <button type="button" onClick={signInUser}>
+                <button className="btn-el" type="button" onClick={signInUser}>
                     Sign In with Google
                 </button>
             )}
