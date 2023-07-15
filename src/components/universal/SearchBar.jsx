ddd140688f339c057e7d383b4e4012618617e711
bst003 <bstanner33@yahoo.com>
2023-07-15 00:17:47 -0400

    Set up mobile header layout and mobile search toggle

--- src/components/universal/SearchBar.jsx
+++ src/components/universal/SearchBar.jsx
@@ -1,22 +1,43 @@
 import "./SearchBar.scss";
 
 const SearchBar = () => {
+    const toggleSearchForm = (e) => {
+        e.preventDefault();
+        const toggle = e.currentTarget;
+        const form = toggle.nextElementSibling;
+
+        if (toggle.classList.contains("active")) {
+            toggle.classList.remove("active");
+            form.classList.remove("active");
+        } else {
+            toggle.classList.add("active");
+            form.classList.add("active");
+        }
+    };
+
     return (
-        <form className="site-search-bar">
-            <label htmlFor="search">
-                <input
-                    id="search"
-                    name="search"
-                    className="search-input"
-                    type="text"
-                    placeholder="Search Posts"
-                />
-                <button type="submit">
-                    <i className="fas fa-search"></i>
-                    <span className="screen-reader-text">Search</span>
-                </button>
-            </label>
-        </form>
+        <div className="site-search-bar">
+            <button className="site-search-bar__toggle" onClick={toggleSearchForm} type="button">
+                <i className="fas fa-search"></i>
+                <span className="screen-reader-text">Toggle Search</span>
+            </button>
+
+            <form className="site-search-bar__form">
+                <label htmlFor="search">
+                    <input
+                        id="search"
+                        name="search"
+                        className="search-input"
+                        type="text"
+                        placeholder="Search Posts"
+                    />
+                    <button className="submit" type="submit">
+                        <i className="fas fa-search"></i>
+                        <span className="screen-reader-text">Search</span>
+                    </button>
+                </label>
+            </form>
+        </div>
     );
 };
 
