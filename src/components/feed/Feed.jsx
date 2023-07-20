f084fffebd088645ea0410f34b2ac5bb82660cde
bst003 <bstanner33@yahoo.com>
2023-07-19 22:40:50 -0400

    Refacror getPosts method and move to own file in firebase directory

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import { collection, getFirestore, getDocs, limit, query } from "firebase/firestore/lite";
+import { getPosts } from "../../firebase/post.js";
 
 import "./Feed.scss";
 
@@ -14,46 +14,9 @@ Parts of Feed
 
 const Feed = () => {
     const [posts, setPosts] = useState([]);
-    const getPosts = async (userGoogleId = null, subRippleId = null) => {
-        try {
-            if (userGoogleId) {
-                console.log(userGoogleId);
-            }
-
-            if (subRippleId) {
-                console.log(subRippleId);
-            }
-
-            const postsQuery = query(collection(getFirestore(), "posts"), limit(10));
-
-            const postsQuerySnapshot = await getDocs(postsQuery);
-
-            const postsArray = [];
-            postsQuerySnapshot.forEach((doc) => {
-                const postObj = {
-                    id: doc.id,
-                    title: doc.data().title,
-                    content: doc.data().content,
-                    userGoogleId: doc.data().userGoogleId,
-                };
-
-                console.log(postObj);
-
-                postsArray.push(postObj);
-            });
-
-            setPosts(postsArray);
-        } catch (error) {
-            console.log("Error fetching posts: " + error);
-        }
-    };
 
     useEffect(() => {
-        getPosts();
-        // const requestedPosts = getPosts();
-        // console.log(requestedPosts);
-
-        // setPosts(requestedPosts);
+        getPosts(null, null, null, setPosts);
     }, []);
 
     return (
---
+++ src/firebase/post.js
@@ -0,0 +1,41 @@
+import { collection, getFirestore, getDocs, limit, query } from "firebase/firestore/lite";
+
+const getPosts = async (userGoogleId = null, subRippleId = null, offset = null, setPostState) => {
+    try {
+        if (userGoogleId) {
+            console.log(userGoogleId);
+        }
+
+        if (subRippleId) {
+            console.log(subRippleId);
+        }
+
+        if (offset) {
+            console.log(offset);
+        }
+
+        const postsQuery = query(collection(getFirestore(), "posts"), limit(10));
+
+        const postsQuerySnapshot = await getDocs(postsQuery);
+
+        const postsArray = [];
+        postsQuerySnapshot.forEach((doc) => {
+            const postObj = {
+                id: doc.id,
+                title: doc.data().title,
+                content: doc.data().content,
+                userGoogleId: doc.data().userGoogleId,
+            };
+
+            console.log(postObj);
+
+            postsArray.push(postObj);
+        });
+
+        setPostState(postsArray);
+    } catch (error) {
+        console.log("Error fetching posts: " + error);
+    }
+};
+
+export { getPosts };
