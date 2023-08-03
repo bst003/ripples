5202e55bb4e0ab0c43dd05d68a2d875c6ca717b4
bst003 <bstanner33@yahoo.com>
2023-08-02 23:42:21 -0400

    Set up AddPost modal controls

--- src/components/feed/add-post/AddPost.jsx
+++ src/components/feed/add-post/AddPost.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
+
 import AddPostModal from "./AddPostModal";
 
 const AddPost = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const closeModal = () => {
+        console.log("close");
+        setModalIsOpen(false);
+    };
+
     return (
         <>
             <div className="add-post-contain">
-                <button className="btn-el" type="button">
+                <button className="btn-el" onClick={() => setModalIsOpen(true)} type="button">
                     Add Post
                 </button>
             </div>
-            <AddPostModal modalIsOpen={false} closeModal={() => console.log("test")} />
+            <AddPostModal modalIsOpen={modalIsOpen} closeModal={closeModal} />
         </>
     );
 };
