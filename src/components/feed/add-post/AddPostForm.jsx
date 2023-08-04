24edd2c25ee6f31a7592c1ce784b8018a6895b71
bst003 <bstanner33@yahoo.com>
2023-08-04 11:27:25 -0400

    Continue AddPostForm build

--- src/components/feed/add-post/AddPostForm.jsx
+++ src/components/feed/add-post/AddPostForm.jsx
@@ -8,11 +8,18 @@ const AddPostForm = () => {
     const populateSubRipplesInput = () => {
         const subRipplesArr = [];
 
+        subRipplesArr.push(
+            <option key="00000001" defaultValue value>
+                {" "}
+                -- select an option --{" "}
+            </option>
+        );
+
         if (subRipples) {
             subRipples.map((subRipple) => {
                 const option = (
                     <option key={subRipple.id} value={subRipple.slug}>
-                        {subRipple.labek}
+                        {subRipple.label}
                     </option>
                 );
                 subRipplesArr.push(option);
@@ -29,20 +36,32 @@ const AddPostForm = () => {
     };
 
     return (
-        <form onSubmit={submitPost}>
+        <form className="add-post-form" onSubmit={submitPost}>
             <div className="form-field">
-                <label>What Sub Ripple are you posting to?</label>
-                <select>{populateSubRipplesInput()}</select>
+                <label htmlFor="sub-ripple">
+                    What Sub Ripple are you posting to? <span className="required">(Required)</span>
+                </label>
+                <select id="sub-ripple" name="sub-ripple">
+                    {populateSubRipplesInput()}
+                </select>
             </div>
 
             <div className="form-field">
-                <label htmlFor="post-title">Post Title</label>
+                <label htmlFor="post-title">
+                    Post Title <span className="required">(Required)</span>
+                </label>
                 <input id="post-title" name="post-title" type="text" />
             </div>
 
             <div className="form-field">
                 <label htmlFor="post-content">Post Content</label>
                 <textarea id="post-content" name="post-content"></textarea>
+            </div>
+
+            <div className="form-field form-footer">
+                <button className="btn-el" type="submit">
+                    Submit
+                </button>
             </div>
         </form>
     );
